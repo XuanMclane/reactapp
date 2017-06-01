bb4f532e2869446c1b453072bdd7930739ed20b1
XuanMclane <xuanmclane@gmail.com>
2017-05-31 20:56:40 -0400

    finish fisrt app

--- countdownchamp/src/App.jsx
+++ countdownchamp/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Clock from './Clock'
+import { Form, FormControl, Button } from "react-bootstrap"
 
 class App extends React.Component{
 	constructor(props) {
@@ -20,10 +21,10 @@ class App extends React.Component{
 			<div className="App">
 				<div className="App-title">CountDown to {this.state.deadline}</div>
 				<Clock deadline={this.state.deadline}/>
-				<div>
-					<input placeholder="new date" onChange={event => this.setState({newDeadline: event.target.value})}/>
-					<button onClick={this.changeDeadline.bind(this)}>Submit</button>
-				</div>
+				<Form inline>
+					<FormControl className="Deadline-input" placeholder="new date" onChange={event => this.setState({newDeadline: event.target.value})}/>
+					<Button onClick={this.changeDeadline.bind(this)}>Submit</Button>
+				</Form>
 			</div>
 			);
 		
